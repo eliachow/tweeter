a9d803009cab1650ff61f48bebecc6d75719dfd5
eliachow <eliacchow@gmail.com>
2023-03-19 09:56:50 -0700

    apply text area character countdown from 140. If less than 0 css color turns red.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
 
-  //listen for event to count down characters from 140 in text area
+  //count down characters from 140 in text area
   let characterCount = 0;
   $("textarea").on("input", function() {
     characterCount = 140 - (this.value.length);
-    console.log("characterCount: ", characterCount);
+    $(this).parent().siblings().children("output").html(characterCount);
+    // $(".counter").html(characterCount)
+
+    //change negative numbers to red
+    if (characterCount < 0) {
+      $(this).parent().siblings().children("output").css("color", "red");
+    } else {
+      $(this).parent().siblings().children("output").css("color", "#545149");
+    }
   });
   
 });
