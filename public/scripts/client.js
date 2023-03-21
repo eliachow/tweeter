f12e163434aa21d597070c5ab771a76fb9d32919
eliachow <eliacchow@gmail.com>
2023-03-21 08:49:33 -0700

    create renderTweets function to loop through tweet data and render article element for each tweet.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  
+
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  //render each tweet from data array
+  const renderTweets = function(tweets) {
+  // loops through tweets
+    for (const element of tweets) {
+      // calls createTweetElement for each tweet
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append(createTweetElement(element));
+    }
+  };
+
+  //render article element for each tweet
   function createTweetElement(tweetData) {
     const $tweet = $(`<article class="container tweet">
         <header>
@@ -27,29 +64,10 @@ $(document).ready(function() {
         </footer>
       </article>`);
   
-    // return <article>
+    // return rendered <article>
     return $tweet;
   }
 
-
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
-
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+  renderTweets(data);
 
 });
